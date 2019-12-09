beec4b463ca71965286d6067f2ef71b927c79d17
dogusriddle <dogus@riddleandcode.com>
2019-12-09 09:22:23 +0000

    added randon bus number for ses tests instead of bus #2 always and added color

--- run.sh
+++ run.sh
@@ -1,13 +1,23 @@
 
 
 
-
+tput setaf 1
 ./bus 0
+tput sgr0
 ./tests/v1_tests
 
+tput setaf 1
 ./bus 1
+tput sgr0
 sudo ./tests/v2_tests
 
+tput setaf 1
 ./bus 2
+tput sgr0
 ./tests/v3_tests
+
+r=$(( $RANDOM % 3 ))
+tput setaf 1
+./bus $r
+tput sgr0
 ./tests/ses_tests
